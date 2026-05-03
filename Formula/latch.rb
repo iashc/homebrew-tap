54493ca31ce15d43de7b0423f7a8f57e94364b15
Ash <dev@ashc.me>
2026-05-03 15:34:56 +0800

    Update latch to v0.1.0

--- Formula/latch.rb
+++ Formula/latch.rb
@@ -2,7 +2,7 @@ class Latch < Formula
   desc "Local-first bookmark service and personal clients"
   homepage "https://github.com/iashc/latch"
   url "https://github.com/iashc/latch/releases/download/v0.1.0/latch-cli-aarch64-apple-darwin.tar.gz"
-  sha256 "715cc1650d8789cf16c8e20b53bc5b756f55588e52dbccb54bafbad0dcb56304"
+  sha256 "e3884d849ba1ce38826b4c94064e88d2093d532999250a5ee27fecd51bd3a986"
   version "0.1.0"
   license "MIT"
 
